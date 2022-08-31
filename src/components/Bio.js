3251d22df699b22a641525304b5c67e396d2f6bc
ISMAIL7866 <92500302+ISMAIL7866@users.noreply.github.com>
2022-08-31 22:45:18 +0530

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ const Bio = _ => {
    
     return (
         <section>
-            <img src={dp} allt ="My profile"/>
+            <img src={dp} alt ="My profile" />
         <h2>Md Ismail</h2>
      <h4>I write Magic ❤</h4>
         </section>
